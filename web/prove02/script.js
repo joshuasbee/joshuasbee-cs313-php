62bc61c8d25e4ee61f12b9cc56eb8cba21597e4d
Joshua Bee <joshuabee@gmail.com>
2021-01-13 18:39:08 -0800

    updated assignments page

--- web/prove02/script.js
+++ web/prove02/script.js
@@ -1,11 +1,13 @@
 function changeColor() {
   var body = document.getElementById("bd");
-  var header = document.getElementById("div");
+  var div = document.getElementById("div");//for the entire body
+  var a = document.getElementById("linky");//for link to other page
   var r = getRGB();
   var g = getRGB();
   var b = getRGB();
   body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-  header.style.color = `rgb(${255 - r}, ${255 - g}, ${255 - b})`;
+  div.style.color = `rgb(${255 - r}, ${255 - g}, ${255 - b})`;
+  a.style.color = `rgb(${255 - r}, ${255 - g}, ${255 - b})`;
 }
 
 function getRGB() {

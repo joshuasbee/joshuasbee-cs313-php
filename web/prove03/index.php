32e200a17b7277dcb2ae3c770166c43a37a26175
Joshua Bee <joshuabee@gmail.com>
2021-01-20 17:38:51 -0800

    Migrated html to the php file and testing

--- web/prove03/index.php
+++ web/prove03/index.php
@@ -1,12 +1,16 @@
-<!-- 
+<?php 
+session_start();
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
-    
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!- - Bootstrap CSS - ->
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <link rel="stylesheet" href="style.css">
@@ -15,7 +19,7 @@
   <body>
     <span class="float-right">
       <form action="cart.php" method='GET'>
-       <!- - <button class='btn btn-success'>Cart</button>  - ->
+       <!-- <button class='btn btn-success'>Cart</button>  -->
        <input type="submit" value="Cart" class='btn-success'/>
       </form>
     </span>
@@ -54,4 +58,26 @@
     </div>
     <script src='script.js'></script>
   </body>
-</html> -->
+</html>
+​
+<?php //empty session
+if (isset($_GET["destroy"])){
+  session_destroy();
+}
+?>
+
+<!-- Check if one of the items was sent by post -->
+<?php
+
+if(isset($_POST['item1'])){
+  $_SESSION["item1"] = $_POST['item1'];
+}
+
+if(isset($_POST['item2'])){
+  $_SESSION["item2"] = $_POST['item2'];
+}
+
+// echo variable info
+echo "Session";
+var_dump($_SESSION);
+?>

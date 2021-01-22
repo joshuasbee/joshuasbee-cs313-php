3fef6879bbb3468c38efd866a208cb27272547b9
Joshua Bee <joshuabee@gmail.com>
2021-01-22 11:35:49 -0800

    testing fix for checkout.php

--- web/prove03/confirmation.php
+++ web/prove03/confirmation.php
@@ -1,5 +1,26 @@
 <?php
 if (!isset($_SESSION)) { session_start(); }
+
+echo '<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="style.css">
+  <title>Purchase received!</title>
+</head>
+<body>
+  <a href="index.php"><- Back to store</a>
+</body>
+</html>';
+
   echo 'Items purchased:<br>';
   if(isset($_SESSION['Anduril'])){
     echo $_SESSION["Anduril"] . '<br>';
@@ -29,22 +50,3 @@ if (!isset($_SESSION)) { session_start(); }
     htmlspecialchars($_GET['zip']) . '<br>';
 ?>
 
-<!doctype html>
-<html lang="en">
-
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-  <link rel="stylesheet" href="style.css">
-  <title>Purchase received!</title>
-</head>
-<body>
-  <a href="index.php"><- Back to store</a>
-</body>
-</html>

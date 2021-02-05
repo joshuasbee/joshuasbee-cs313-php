19f099c0c95fb39e1cde3cef0f3bdb3178a67d34
Joshua Bee <joshuabee@gmail.com>
2021-02-05 10:16:58 -0800

    commit from script

--- web/prove05/search.php
+++ web/prove05/search.php
@@ -9,11 +9,10 @@ $query = ucwords($query);//Capitalizes each word, that is how it is in database
 // echo "searched for: " . "$query"; // This works fine
 $stmt = $db->prepare("SELECT * FROM items WHERE item_name LIKE '%$query%");
 $stmt->execute();
-
-// Go through each result
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-{
-  $names = $row['item_name'];
-  echo "Results: <br>" . "$names";
-}
+echo $stmt->fetch(PDO::FETCH_ASSOC);
+// while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+// {
+//   $names = $row['item_name'];
+//   echo "Results: <br>" . "$names";
+// }
 ?>

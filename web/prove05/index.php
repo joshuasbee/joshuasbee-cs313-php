75eadd0147706d9ad66b952cae028846931b608a
Joshua Bee <joshuabee@gmail.com>
2021-02-05 09:27:20 -0800

    commit from script

--- web/prove05/index.php
+++ web/prove05/index.php
@@ -28,14 +28,12 @@ if (!isset($_SESSION)) { session_start(); }
   </span>
 <!--  -->
   <div class="input-group rounded" id="search">
-  <div class="input-group col-md-4">
-            <input class="form-control py-2 border-right-0 border" type="search" value="search" id="example-search-input">
-            <span class="input-group-append">
-              <button class="btn btn-outline-secondary border-left-0 border" type="button">
-                    <i class="far fa-search"></i>
-              </button>
-            </span>
-        </div>
+  <input class="form-control py-2" type="search" value="search" id="example-search-input">
+      <span class="input-group-append">
+        <button class="btn btn-outline-secondary" type="button">
+            <i class="fa fa-search"></i>
+        </button>
+      </span>
 </div>
 <!--  -->
   <h1 class="text-center">LOTR item shop</h1>

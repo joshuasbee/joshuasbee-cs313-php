f9639ec5e0f15a015e7823366f16e54bcf4a3e2e
Joshua Bee <joshuabee@gmail.com>
2021-02-05 09:22:41 -0800

    commit from script

--- web/prove05/index.php
+++ web/prove05/index.php
@@ -30,8 +30,8 @@ if (!isset($_SESSION)) { session_start(); }
   <div class="input-group rounded" id="search">
   <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
     aria-describedby="search-addon" >
-  <span class="input-group-text border-0" id="search-addon">
-    <i class="fas fa-search"></i>
+    <button class="btn btn-secondary" type="button">
+    <i class="fa fa-search"></i>
   </span>
 </div>
 <!--  -->

4084fc0f1134c532626b16bd84dc243e3ce95b42
Joshua Bee <joshuabee@gmail.com>
2021-02-05 10:07:28 -0800

    commit from script

--- web/prove05/search.php
+++ web/prove05/search.php
@@ -4,8 +4,9 @@ $db = get_db();
 if (!isset($_SESSION)) { session_start(); }
 
 $query = $_GET['query'];
+echo "searched for: " . "$query";
 $query = htmlspecialchars($query);//filter out <script> or other malicious code
-$stmt = $db->prepare("SELECT * FROM items WHERE item_name LIKE '%$query%");//Select * allows me to pick different rows of the table in the while loop
+$stmt = $db->prepare("SELECT * FROM items WHERE item_name LIKE '%$query%");
 $stmt->execute();
 
 // Go through each result
@@ -14,6 +15,4 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
   $names = $row['item_name'];
   echo "Results: <br>" . "$names";
 }
-
-
 ?>

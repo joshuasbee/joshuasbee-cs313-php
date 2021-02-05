cf48999a1c5a07e5d35b204d889041014fa8c890
Joshua Bee <joshuabee@gmail.com>
2021-02-04 16:20:21 -0800

    commit from script

--- web/prove05/index.php
+++ web/prove05/index.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION)) { session_start(); }
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="style.css">
+  <!-- <link rel="stylesheet" href="style.css"> -->
   <title>LOTR store</title>
 </head>
 

19d7ac170ef644c8fd9c2e4c4dc53f7e7c945107
Joshua Bee <joshuabee@gmail.com>
2021-02-05 16:04:56 -0800

    commit from script

--- web/prove05/index.php
+++ web/prove05/index.php
@@ -47,6 +47,7 @@ if (!isset($_SESSION)) { session_start(); }
   $stmt = $db->prepare("SELECT * FROM items");//Select * allows me to pick different rows of the table in the while loop
   $stmt->execute();
   $iter_count = 0;
+  echo "<form action=item_info.php method='get";
   //maybe define some constants like 1st row length
   while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
   {
@@ -55,13 +56,16 @@ if (!isset($_SESSION)) { session_start(); }
     if($iter_count <= 4) { echo "<div class='col'>"; }
     $pic = $row['image_dir'];
     $names = $row['item_name'];
-    echo "<img id='$names' src='$pic'><p>$names</p><br>";
-    //TODO add an add to cart button here maybe
+    echo "<input type='image' id='$names' src='$pic'><p>$names</p><br>";
+    
+    //TODO add an add to cart button here maybe?
+
+    //click image for item info page NAME is the way to get variable using $_GET['name']
     if($iter_count <= 4){ echo "</div>"; }//closes each col
     if($iter_count == 4){ echo "</div>"; }//closes first row
     $iter_count++;
   }
-
+  echo "</form>";
 ?>
 
 </div><!-- container closing tag -->
--- web/prove05/search.php
+++ web/prove05/search.php
@@ -4,14 +4,13 @@ $db = get_db();
 if (!isset($_SESSION)) { session_start(); }
 
 $query = $_GET['query'];
-echo "Results for search of \"$query\":<br>";//show before capitalized
-$query = htmlspecialchars($query);//filter out <script> or other malicious code
-$query = ucwords($query);//Capitalizes each word, that is how it is in database
-// echo "searched for: " . "$query"; // This works fine
 if ($query != ""){
+  echo "Results for search of \"$query\":<br>";//show before capitalized
+  $query = htmlspecialchars($query);//filter out <script> or other malicious code
+  $query = ucwords($query);//Capitalizes each word, that is how it is in database
+  // echo "searched for: " . "$query"; // This works fine
   $stmt = $db->prepare("SELECT * FROM items WHERE item_name LIKE '%$query%'");
   $stmt->execute();
-
   while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
   {
     $name = $row['item_name'];

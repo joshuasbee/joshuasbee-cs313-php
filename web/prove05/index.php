5cc462a561727dfbdd6df09255e8a59096041d4e
Joshua Bee <joshuabee@gmail.com>
2021-02-06 12:04:31 -0800

    commit from script

--- web/prove05/index.php
+++ web/prove05/index.php
@@ -55,10 +55,10 @@ if (!isset($_SESSION)) { session_start(); }
   {
     if($iter_count == 0){ echo "<div class='row'>"; }
     if($iter_count == 3){ echo "</div><div class='row'>"; }//closes first row and opens next
-    if($iter_count <= 4) { echo "<div class='col'>"; }
+    if($iter_count <= 4) { echo "<div class='col justify-content-center'>"; }
     $pic = $row['image_dir'];
     $names = $row['item_name'];
-    echo "<button id='$names' value='$names' name='$names'><img src='$pic' class='justify-content-center'></button>";
+    echo "<button id='$names' value='$names' name='$names'><img src='$pic'></button>";
     $names = ucwords($names);
     echo "<p>$names</p><br>";//name property for using $_GET['item']
     //TODO add an add to cart button here maybe?

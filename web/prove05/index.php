1e37f44eba732f1c6dec5dd4d9c7ddf2578b93ef
Joshua Bee <joshuabee@gmail.com>
2021-02-06 13:44:17 -0800

    commit from script

--- web/prove05/index.php
+++ web/prove05/index.php
@@ -29,7 +29,6 @@ if (!isset($_SESSION)) { session_start(); }
       <input type="submit" value="Cart" class='btn-success' />
     </form>
   </span>
-<!--  -->
 <form action="search.php" method='get'>
   <div class="input-group rounded mx-auto" id="search">  <!-- mx-auto is the only thing that I found to work to center this -->
   <input class="form-control py-2 border-right-0 border" type="search" placeholder="search" name="query" id="example-search-input">
@@ -45,7 +44,7 @@ if (!isset($_SESSION)) { session_start(); }
   
 <?php
   echo "<form action='item_info.php' method='get'";
-  echo "<div class='container'>";
+  echo "<div class='container text-center'>";
   $stmt = $db->prepare("SELECT * FROM items");//Select * allows me to pick different rows of the table in the while loop
   $stmt->execute();
   $iter_count = 0;
@@ -55,15 +54,14 @@ if (!isset($_SESSION)) { session_start(); }
   {
     if($iter_count == 0){ echo "<div class='row'>"; }
     if($iter_count == 3){ echo "</div><div class='row'>"; }//closes first row and opens next
-    if($iter_count <= 4) { echo "<div class='col justify-content-center'>"; }
+    if($iter_count <= 4) { echo "<div class='col'>"; }
     $pic = $row['image_dir'];
     $names = $row['item_name'];
     echo "<button id='$names' value='$names' name='$names'><img src='$pic'></button>";
     $names = ucwords($names);
     echo "<p>$names</p><br>";//name property for using $_GET['item']
-    //TODO add an add to cart button here maybe?
+    // justify-content-center somewhere around here???????????
 
-    //click image for item info page NAME is the way to get variable using $_GET['name']
     if($iter_count <= 4){ echo "</div>"; }//closes each col
     if($iter_count == 4){ echo "</div>"; }//closes first row
     $iter_count++;

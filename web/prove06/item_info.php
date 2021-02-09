ad5a59cc349d67c7c7affda8ba90a7861eb8ebfb
Joshua Bee <joshuabee@gmail.com>
2021-02-09 14:45:05 -0800

    commit from script

---
+++ web/prove06/item_info.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<?php 
+  require "../db/dbConnect.php";
+  $db = get_db();
+  if (!isset($_SESSION)) { session_start(); }
+  //lots of if(isset($_GET['Anduril'])) {$query = $_GET['Anduril'];}
+  if(isset($_GET['anduril'])){$query = $_GET['anduril'];}
+  elseif(isset($_GET['glamdring'])){$query = $_GET['glamdring'];}
+  elseif(isset($_GET['sting'])){$query = $_GET['sting'];}
+  elseif(isset($_GET['lego_gandalf'])){$query = $_GET['lego_gandalf'];}//something changes spaces to _ 
+  elseif(isset($_GET['orc_armor'])){$query = $_GET['orc_armor'];}
+  // else{echo var_dump($_GET);}
+
+  $query = htmlspecialchars($query);//filter out <script> or other malicious code
+  $stmt = $db->prepare("SELECT * FROM items WHERE item_name = '$query'");
+  $stmt->execute(); ?>
+
+  <div class="container"> <!-- justify-content-center"> -->
+  <?php
+  while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+  {
+    $name = $row['item_name'];
+    $pic = $row['image_dir'];
+    $stock = $row['quantity'];
+    $price = $row['price'];
+    $nameUC = ucwords($name);//capitalize the name of the item for display
+    echo "<div class='row justify-content-center'>";//justify-content-center must be on each item within the container div
+    echo "<img src='$pic'></div>" . "<div class='row justify-content-center'>$nameUC</div>";
+    echo "<div class='row justify-content-center'>Price: \$$price</div><div class='row justify-content-center'>";
+    if ($stock >= 10){ echo "In stock"; }
+    elseif ($stock > 0 && $stock < 10){ echo "<p class='text-warning'>Low stock</p>"; }
+    else { echo "<p class='text-danger'>Out of stock</p>"; }
+    echo "</div>";//closes row for stock
+    if ($stock > 0){
+      echo "<div class='row justify-content-center'>";
+      echo "<button id='$name' value='$name' name='$name' class='rounded btn-success'>Add to cart</button>";
+      echo "</div>";
+    }
+    
+  }
+?>
+</div>
+
+</body>
+</html>
---
+++ web/prove06/search.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<?php 
+  require "../db/dbConnect.php";
+  $db = get_db();
+  if (!isset($_SESSION)) { session_start(); }
+
+  $query = $_GET['query'];
+  $query = htmlspecialchars($query);//filter out <script> or other malicious code
+  $query = lcfirst($query);//Lowercase each word like in database
+
+  $stmt = $db->prepare("SELECT * FROM items WHERE item_name LIKE '%$query%'");
+  $stmt->execute();
+  while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+  {
+    $name = $row['item_name'];
+    $pic = $row['image_dir'];
+    $stock = $row['quantity'];
+    $price = $row['price'];
+    $name = ucwords($name);
+    echo "<img src='$pic'>" . "<div>$name</div>" . "Price: \$$price<br>";
+    if ($stock >= 10){ echo "In stock"; }
+    elseif ($stock > 1 && $stock < 10){ echo "Low stock"; }
+    else { echo "Out of stock"; }
+    if ($stock > 0){
+      echo "<br><button id='$name' value='$name' name='$name' class='rounded btn-success'>Add to cart</button>";
+    }
+    echo "<br>";
+  }
+?>
+</body>
+</html>

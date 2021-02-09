322a8448d0d284a8553450aca615bb4aefe38d93
Joshua Bee <joshuabee@gmail.com>
2021-02-09 15:01:49 -0800

    commit from script

--- web/prove06/login.php
+++ web/prove06/login.php
@@ -14,15 +14,15 @@
   <title>Login</title>
 </head>
 <body>
-<div class='container'>
-  <div class='row justify-content-center align-items-center'>
-    <p>Sign in</p><br>
+<div class='container'> 
+  <h1 class='justify-content-center'>Sign in</h1>
+    <div class='row justify-content-center align-items-center'>
     <form action="" method="">
       <input type='text' placeholder='email' class='form-control'>
       <br>
       <input type='password' placeholder='password' class='form-control'>
       <br>
-      <button type='submit' class='btn btn-info'>
+      <button type='submit' class='btn btn-info'>Login</button>
     </form>
   </div>
 </div>
